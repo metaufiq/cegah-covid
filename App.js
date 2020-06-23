6c2753e26ba72f162003340f89d0a2cff6359ebc
taufiq <taufiq1689@gmail.com>
2020-06-23 15:21:52 +0800

    table for covid-19 per province done

--- App.js
+++ App.js
@@ -32,27 +32,30 @@ export default class App extends React.Component {
 
 	render() {
 		return (
-			<View style={{ paddingHorizontal: 10, paddingVertical: 10, flex: 1 }}>
+			<View style={{  flex: 1 }}>
 				<View
 					style={{
 						flex: 4,
 						flexDirection: 'row',
 						paddingVertical: 30,
 						justifyContent: 'center',
-						alignItems: 'center'
+						alignItems: 'center',
+						paddingRight:5,
+						borderBottomWidth:1,
+						marginBottom:2
 					}}
 				>
 					<View style={{ flex: 1 }}>
-						<Text style={{ textAlign: 'center' }}>Provinsi</Text>
+						<Text style={{ textAlign: 'center', fontWeight:'bold' }}>Provinsi</Text>
 					</View>
 					<View style={{ flex: 1 }}>
-						<Text style={{ textAlign: 'center' }}>Positif</Text>
+						<Text style={{ textAlign: 'center', fontWeight:'bold' }}>Positif</Text>
 					</View>
 					<View style={{ flex: 1 }}>
-						<Text style={{ textAlign: 'center' }}>Sembuh</Text>
+						<Text style={{ textAlign: 'center', fontWeight:'bold' }}>Sembuh</Text>
 					</View>
 					<View style={{ flex: 1 }}>
-						<Text style={{ textAlign: 'center' }}>Meninggal</Text>
+						<Text style={{ textAlign: 'center', fontWeight:'bold' }}>Meninggal</Text>
 					</View>
 				</View>
 				<FlatList
@@ -68,22 +71,23 @@ export default class App extends React.Component {
 								style={{
 									flex: 4,
 									flexDirection: 'row',
-									paddingVertical: 20,
 									justifyContent: 'center',
-									alignItems: 'center'
+									alignItems: 'center',
+									marginBottom:2,
+									paddingRight:5
 								}}
 							>
 								<View style={{ flex: 1 }}>
-									<Text style={{ textAlign: 'center' }}>{item.Provinsi}</Text>
+									<Text style={{ textAlign: 'center',color:'#807B7B',paddingHorizontal:5 }}>{item.Provinsi}</Text>
 								</View>
-								<View style={{ flex: 1 }}>
-									<Text style={{ textAlign: 'center' }}>{item.Kasus_Posi}</Text>
+								<View style={{ flex: 1, backgroundColor:'#FEBE00', height:'100%', minHeight:70, justifyContent:'center', alignContent:'center' }}>
+									<Text style={{ textAlign: 'center', color:'white' }}>{item.Kasus_Posi}</Text>
 								</View>
-								<View style={{ flex: 1 }}>
-									<Text style={{ textAlign: 'center' }}>{item.Kasus_Semb}</Text>
+								<View style={{ flex: 1, backgroundColor:'#5CB75C', height:'100%', minHeight:70, justifyContent:'center', alignContent:'center',marginHorizontal:1 }}>
+									<Text style={{ textAlign: 'center', color:'white' }}>{item.Kasus_Semb}</Text>
 								</View>
-								<View style={{ flex: 1 }}>
-									<Text style={{ textAlign: 'center' }}>{item.Kasus_Meni}</Text>
+								<View style={{ flex: 1, backgroundColor:'#D9534F', height:'100%', minHeight:70, justifyContent:'center', alignContent:'center' }}>
+									<Text style={{ textAlign: 'center', color:'white' }}>{item.Kasus_Meni}</Text>
 								</View>
 							</View>
 						);
