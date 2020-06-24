b2306d63cbe53f947d5897b715c47397d25cad80
taufiq <taufiq1689@gmail.com>
2020-06-24 10:57:50 +0800

    add activity indikator to provinsi and world view

--- src/ProvincesCovidComponent.js
+++ src/ProvincesCovidComponent.js
@@ -8,11 +8,12 @@
 
 import React from 'react';
 import { FlatList, View } from 'react-native';
-import { Text } from 'react-native-paper';
+import { Text, ActivityIndicator } from 'react-native-paper';
 
 export default class ProvincesCovidComponent extends React.Component {
 	state = {
-		data: []
+		data: [],
+		isLoadData: false
 	};
 
 	ambilData = () => {
@@ -21,7 +22,7 @@ export default class ProvincesCovidComponent extends React.Component {
 		)
 			.then((response) => response.json())
 			.then((json) => {
-				this.setState({ data: json.features });
+				this.setState({ data: json.features, isLoadData: true });
 			})
 			.catch((error) => {
 				console.error(error);
@@ -33,99 +34,104 @@ export default class ProvincesCovidComponent extends React.Component {
 
 	render() {
 		return (
-			<View style={{ flex: 1,backgroundColor:'white' }}>
-				<View
-					style={{
-						flex: 4,
-						flexDirection: 'row',
-						paddingVertical: 30,
-						justifyContent: 'center',
-						alignItems: 'center',
-						paddingRight: 5,
-						elevation: 1,
-						borderBottomColor: '#807B7B',
-						marginBottom: 2
-					}}
-				>
-					<View style={{ flex: 1 }}>
-						<Text style={{ textAlign: 'center', fontWeight: 'bold' }}>Provinsi</Text>
+			<View style={{ flex: 1, backgroundColor: 'white' }}>
+				<View style={{ display: this.state.isLoadData ? 'flex' : 'none' }}>
+					<View
+						style={{
+							flex: 4,
+							flexDirection: 'row',
+							paddingVertical: 30,
+							justifyContent: 'center',
+							alignItems: 'center',
+							paddingRight: 5,
+							elevation: 1,
+							borderBottomColor: '#807B7B',
+							marginBottom: 2
+						}}
+					>
+						<View style={{ flex: 1 }}>
+							<Text style={{ textAlign: 'center', fontWeight: 'bold' }}>Provinsi</Text>
+						</View>
+						<View style={{ flex: 1 }}>
+							<Text style={{ textAlign: 'center', fontWeight: 'bold' }}>Positif</Text>
+						</View>
+						<View style={{ flex: 1 }}>
+							<Text style={{ textAlign: 'center', fontWeight: 'bold' }}>Sembuh</Text>
+						</View>
+						<View style={{ flex: 1 }}>
+							<Text style={{ textAlign: 'center', fontWeight: 'bold' }}>Meninggal</Text>
+						</View>
 					</View>
-					<View style={{ flex: 1 }}>
-						<Text style={{ textAlign: 'center', fontWeight: 'bold' }}>Positif</Text>
-					</View>
-					<View style={{ flex: 1 }}>
-						<Text style={{ textAlign: 'center', fontWeight: 'bold' }}>Sembuh</Text>
-					</View>
-					<View style={{ flex: 1 }}>
-						<Text style={{ textAlign: 'center', fontWeight: 'bold' }}>Meninggal</Text>
-					</View>
-				</View>
-				<FlatList
-					data={this.state.data}
-					renderItem={({ item }) => {
-						item = item.attributes;
+					<FlatList
+						data={this.state.data}
+						renderItem={({ item }) => {
+							item = item.attributes;
 
-						if (item.Provinsi == 'Indonesia') {
-							return;
-						}
-						return (
-							<View
-								style={{
-									flex: 4,
-									flexDirection: 'row',
-									justifyContent: 'center',
-									alignItems: 'center',
-									marginBottom: 2,
-									paddingRight: 5
-								}}
-							>
-								<View style={{ flex: 1 }}>
-									<Text style={{ textAlign: 'center', color: '#807B7B', paddingHorizontal: 5 }}>
-										{item.Provinsi}
-									</Text>
-								</View>
+							if (item.Provinsi == 'Indonesia') {
+								return;
+							}
+							return (
 								<View
 									style={{
-										flex: 1,
-										backgroundColor: '#FEBE00',
-										height: '100%',
-										minHeight: 70,
+										flex: 4,
+										flexDirection: 'row',
 										justifyContent: 'center',
-										alignContent: 'center'
+										alignItems: 'center',
+										marginBottom: 2,
+										paddingRight: 5
 									}}
 								>
-									<Text style={{ textAlign: 'center', color: 'white' }}>{item.Kasus_Posi}</Text>
+									<View style={{ flex: 1 }}>
+										<Text style={{ textAlign: 'center', color: '#807B7B', paddingHorizontal: 5 }}>
+											{item.Provinsi}
+										</Text>
+									</View>
+									<View
+										style={{
+											flex: 1,
+											backgroundColor: '#FEBE00',
+											height: '100%',
+											minHeight: 70,
+											justifyContent: 'center',
+											alignContent: 'center'
+										}}
+									>
+										<Text style={{ textAlign: 'center', color: 'white' }}>{item.Kasus_Posi}</Text>
+									</View>
+									<View
+										style={{
+											flex: 1,
+											backgroundColor: '#5CB75C',
+											height: '100%',
+											minHeight: 70,
+											justifyContent: 'center',
+											alignContent: 'center',
+											marginHorizontal: 1
+										}}
+									>
+										<Text style={{ textAlign: 'center', color: 'white' }}>{item.Kasus_Semb}</Text>
+									</View>
+									<View
+										style={{
+											flex: 1,
+											backgroundColor: '#D9534F',
+											height: '100%',
+											minHeight: 70,
+											justifyContent: 'center',
+											alignContent: 'center'
+										}}
+									>
+										<Text style={{ textAlign: 'center', color: 'white' }}>{item.Kasus_Meni}</Text>
+									</View>
 								</View>
-								<View
-									style={{
-										flex: 1,
-										backgroundColor: '#5CB75C',
-										height: '100%',
-										minHeight: 70,
-										justifyContent: 'center',
-										alignContent: 'center',
-										marginHorizontal: 1
-									}}
-								>
-									<Text style={{ textAlign: 'center', color: 'white' }}>{item.Kasus_Semb}</Text>
-								</View>
-								<View
-									style={{
-										flex: 1,
-										backgroundColor: '#D9534F',
-										height: '100%',
-										minHeight: 70,
-										justifyContent: 'center',
-										alignContent: 'center'
-									}}
-								>
-									<Text style={{ textAlign: 'center', color: 'white' }}>{item.Kasus_Meni}</Text>
-								</View>
-							</View>
-						);
-					}}
-					keyExtractor={(item) => item.attributes.FID}
-				/>
+							);
+						}}
+						keyExtractor={(item) => item.attributes.FID}
+					/>
+				</View>
+				<View style={{ display: this.state.isLoadData ? 'none' : 'flex', justifyContent:'center',alignContent:'center',flex:1 }}>
+					<ActivityIndicator size="large" color="#D9534F" />
+				</View>
 			</View>
 		);
 	}
--- src/WorldCovidComponent.js
+++ src/WorldCovidComponent.js
@@ -8,18 +8,19 @@
 
 import React from 'react';
 import { FlatList, View } from 'react-native';
-import { Text } from 'react-native-paper';
+import { Text, ActivityIndicator } from 'react-native-paper';
 
 export default class WorldCovidComponent extends React.Component {
 	state = {
-		data: []
+		data: [],
+		isLoadData: false
 	};
 
 	ambilData = () => {
 		fetch('https://api.kawalcorona.com/')
 			.then((response) => response.json())
 			.then((json) => {
-				this.setState({ data: json });
+				this.setState({ data: json,isLoadData: true });
 			})
 			.catch((error) => {
 				console.error(error);
@@ -32,98 +33,111 @@ export default class WorldCovidComponent extends React.Component {
 	render() {
 		return (
 			<View style={{ flex: 1, backgroundColor: 'white' }}>
+				<View style={{ display: this.state.isLoadData ? 'flex' : 'none' }}>
+					<View
+						style={{
+							flex: 4,
+							flexDirection: 'row',
+							paddingVertical: 30,
+							justifyContent: 'center',
+							alignItems: 'center',
+							paddingRight: 5,
+							borderBottomColor: '#807B7B',
+							marginBottom: 2,
+							elevation: 1
+						}}
+					>
+						<View style={{ flex: 1 }}>
+							<Text style={{ textAlign: 'center', fontWeight: 'bold' }}>Negara</Text>
+						</View>
+						<View style={{ flex: 1 }}>
+							<Text style={{ textAlign: 'center', fontWeight: 'bold' }}>Positif</Text>
+						</View>
+						<View style={{ flex: 1 }}>
+							<Text style={{ textAlign: 'center', fontWeight: 'bold' }}>Sembuh</Text>
+						</View>
+						<View style={{ flex: 1 }}>
+							<Text style={{ textAlign: 'center', fontWeight: 'bold' }}>Meninggal</Text>
+						</View>
+					</View>
+					<FlatList
+						data={this.state.data}
+						renderItem={({ item }) => {
+							item = item.attributes;
+
+							if (item.Provinsi == 'Indonesia') {
+								return;
+							}
+							return (
+								<View
+									style={{
+										flex: 4,
+										flexDirection: 'row',
+										justifyContent: 'center',
+										alignItems: 'center',
+										marginBottom: 2,
+										paddingRight: 5
+									}}
+								>
+									<View style={{ flex: 1 }}>
+										<Text style={{ textAlign: 'center', color: '#807B7B', paddingHorizontal: 5 }}>
+											{item.Country_Region}
+										</Text>
+									</View>
+									<View
+										style={{
+											flex: 1,
+											backgroundColor: '#FEBE00',
+											height: '100%',
+											minHeight: 70,
+											justifyContent: 'center',
+											alignContent: 'center'
+										}}
+									>
+										<Text style={{ textAlign: 'center', color: 'white' }}>{item.Confirmed}</Text>
+									</View>
+									<View
+										style={{
+											flex: 1,
+											backgroundColor: '#5CB75C',
+											height: '100%',
+											minHeight: 70,
+											justifyContent: 'center',
+											alignContent: 'center',
+											marginHorizontal: 1
+										}}
+									>
+										<Text style={{ textAlign: 'center', color: 'white' }}>{item.Recovered}</Text>
+									</View>
+									<View
+										style={{
+											flex: 1,
+											backgroundColor: '#D9534F',
+											height: '100%',
+											minHeight: 70,
+											justifyContent: 'center',
+											alignContent: 'center'
+										}}
+									>
+										<Text style={{ textAlign: 'center', color: 'white' }}>{item.Deaths}</Text>
+									</View>
+								</View>
+							);
+						}}
+						keyExtractor={(item) => item.attributes.FID}
+					/>
+				</View>
+
 				<View
 					style={{
-						flex: 4,
-						flexDirection: 'row',
-						paddingVertical: 30,
+						display: this.state.isLoadData ? 'none' : 'flex',
 						justifyContent: 'center',
-						alignItems: 'center',
-						paddingRight: 5,
-						borderBottomColor: '#807B7B',
-						marginBottom: 2,
-						elevation: 1
+						alignContent: 'center',
+						flex: 1
 					}}
 				>
-					<View style={{ flex: 1 }}>
-						<Text style={{ textAlign: 'center', fontWeight: 'bold' }}>Negara</Text>
-					</View>
-					<View style={{ flex: 1 }}>
-						<Text style={{ textAlign: 'center', fontWeight: 'bold' }}>Positif</Text>
-					</View>
-					<View style={{ flex: 1 }}>
-						<Text style={{ textAlign: 'center', fontWeight: 'bold' }}>Sembuh</Text>
-					</View>
-					<View style={{ flex: 1 }}>
-						<Text style={{ textAlign: 'center', fontWeight: 'bold' }}>Meninggal</Text>
-					</View>
+					<ActivityIndicator size="large" color="#D9534F" />
 				</View>
-				<FlatList
-					data={this.state.data}
-					renderItem={({ item }) => {
-						item = item.attributes;
-
-						if (item.Provinsi == 'Indonesia') {
-							return;
-						}
-						return (
-							<View
-								style={{
-									flex: 4,
-									flexDirection: 'row',
-									justifyContent: 'center',
-									alignItems: 'center',
-									marginBottom: 2,
-									paddingRight: 5
-								}}
-							>
-								<View style={{ flex: 1 }}>
-									<Text style={{ textAlign: 'center', color: '#807B7B', paddingHorizontal: 5 }}>
-										{item.Country_Region}
-									</Text>
-								</View>
-								<View
-									style={{
-										flex: 1,
-										backgroundColor: '#FEBE00',
-										height: '100%',
-										minHeight: 70,
-										justifyContent: 'center',
-										alignContent: 'center'
-									}}
-								>
-									<Text style={{ textAlign: 'center', color: 'white' }}>{item.Confirmed}</Text>
-								</View>
-								<View
-									style={{
-										flex: 1,
-										backgroundColor: '#5CB75C',
-										height: '100%',
-										minHeight: 70,
-										justifyContent: 'center',
-										alignContent: 'center',
-										marginHorizontal: 1
-									}}
-								>
-									<Text style={{ textAlign: 'center', color: 'white' }}>{item.Recovered}</Text>
-								</View>
-								<View
-									style={{
-										flex: 1,
-										backgroundColor: '#D9534F',
-										height: '100%',
-										minHeight: 70,
-										justifyContent: 'center',
-										alignContent: 'center'
-									}}
-								>
-									<Text style={{ textAlign: 'center', color: 'white' }}>{item.Deaths}</Text>
-								</View>
-							</View>
-						);
-					}}
-					keyExtractor={(item) => item.attributes.FID}
-				/>
 			</View>
 		);
 	}
