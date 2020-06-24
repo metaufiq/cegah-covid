dd8e68de852bf36eb5ed420f07c2a5e684d83479
taufiq <taufiq1689@gmail.com>
2020-06-25 06:31:35 +0800

    change view for country

--- src/CountryCovidComponent.js
+++ src/CountryCovidComponent.js
@@ -30,7 +30,7 @@ export default class CountryCovidComponent extends React.Component {
 	}
 
 	openBNPBWebsite = () => {
-		let url ="https://bnpb.go.id/berita"
+		let url = 'https://bnpb.go.id/berita';
 		Linking.canOpenURL(url).then((supported) => {
 			if (supported) {
 				Linking.openURL(url);
@@ -41,7 +41,7 @@ export default class CountryCovidComponent extends React.Component {
 	};
 
 	openKawalCovidTwitter = () => {
-		let url ="https://twitter.com/kawalcovid19"
+		let url = 'https://twitter.com/kawalcovid19';
 		Linking.canOpenURL(url).then((supported) => {
 			if (supported) {
 				Linking.openURL(url);
@@ -56,68 +56,70 @@ export default class CountryCovidComponent extends React.Component {
 			<View
 				style={{
 					flex: 1,
-					padding: 10,
+
 					backgroundColor: 'white'
 				}}
 			>
-				<View style={{ marginBottom: 20 }}>
-					<Text style={{ textAlign: 'center', fontSize: 40 }}>Kasus Covid-19 Indonesia</Text>
+				<View style={{ marginBottom: 20, flex: 0.8, backgroundColor: '#D9534F',paddingTop:10 }}>
+					<Text style={{ textAlign: 'center', fontSize: 40, color:'white' }}>Kasus Covid-19 Indonesia</Text>
 				</View>
 
-				<Card elevation={3}>
-					<View style={{ padding: 10 }}>
-						<Card.Content>
-							<View style={{ justifyContent: 'center', alignContent: 'center', paddingBottom: 10 }}>
-								<Text style={{ textAlign: 'center', fontSize: 30 }}>Positif</Text>
-								<Text style={{ textAlign: 'center', fontSize: 40, color: '#FEBE00' }}>
-									{this.state.data ? this.state.data.positif : '_'}
-								</Text>
+				<View style={{ flex: 1, padding: 10,marginTop:-180 }}>
+					<Card elevation={3}>
+						<View style={{ padding: 10 }}>
+							<Card.Content>
+								<View style={{ justifyContent: 'center', alignContent: 'center', paddingBottom: 10 }}>
+									<Text style={{ textAlign: 'center', fontSize: 30 }}>Positif</Text>
+									<Text style={{ textAlign: 'center', fontSize: 40, color: '#FEBE00' }}>
+										{this.state.data ? this.state.data.positif : '_'}
+									</Text>
+								</View>
+							</Card.Content>
+							<View style={{ flexDirection: 'row', justifyContent: 'center' }}>
+								<Card.Content>
+									<View style={{ justifyContent: 'center', alignContent: 'center' }}>
+										<Text style={{ textAlign: 'center', fontSize: 30 }}>Sembuh</Text>
+										<Text style={{ textAlign: 'center', fontSize: 40, color: '#5CB75C' }}>
+											{this.state.data.sembuh ? this.state.data.sembuh : '_'}
+										</Text>
+									</View>
+								</Card.Content>
+								<Card.Content>
+									<View style={{ justifyContent: 'center', alignContent: 'center' }}>
+										<Text style={{ textAlign: 'center', fontSize: 30 }}>Meninggal</Text>
+										<Text style={{ textAlign: 'center', fontSize: 40, color: '#D9534F' }}>
+											{this.state.data.meninggal ? this.state.data.meninggal : '_'}
+										</Text>
+									</View>
+								</Card.Content>
 							</View>
-						</Card.Content>
-						<View style={{ flexDirection: 'row', justifyContent: 'center' }}>
-							<Card.Content>
-								<View style={{ justifyContent: 'center', alignContent: 'center' }}>
-									<Text style={{ textAlign: 'center', fontSize: 30 }}>Sembuh</Text>
-									<Text style={{ textAlign: 'center', fontSize: 40, color: '#5CB75C' }}>
-										{this.state.data.sembuh ? this.state.data.sembuh : '_'}
-									</Text>
-								</View>
-							</Card.Content>
-							<Card.Content>
-								<View style={{ justifyContent: 'center', alignContent: 'center' }}>
-									<Text style={{ textAlign: 'center', fontSize: 30 }}>Meninggal</Text>
-									<Text style={{ textAlign: 'center', fontSize: 40, color: '#D9534F' }}>
-										{this.state.data.meninggal ? this.state.data.meninggal : '_'}
-									</Text>
-								</View>
-							</Card.Content>
 						</View>
+					</Card>
+
+					<View style={{ marginVertical: 30 }}>
+						<Button
+							icon="newspaper"
+							mode="contained"
+							onPress={this.openBNPBWebsite}
+							color="#0000A4"
+							style={{ paddingVertical: 10 }}
+						>
+							Berita Covid-19 BNPB
+						</Button>
 					</View>
-				</Card>
 
-				<View style={{ marginVertical: 30 }}>
-					<Button
-						icon="newspaper"
-						mode="contained"
-						onPress={this.openBNPBWebsite}
-						color="#0000A4"
-						style={{ paddingVertical: 10 }}
-					>
-						Berita Covid-19 BNPB
-					</Button>
-				</View>
-
-				<View>
-					<Button
-						icon="twitter"
-						mode="contained"
-						onPress={this.openKawalCovidTwitter}
-						color="#1DA1F2"
-						style={{ paddingVertical: 10 }}
-						labelStyle={{ color: 'white' }}
-					>
-						Twitter KawalCovid19
-					</Button>
+					<View>
+						<Button
+							icon="twitter"
+							mode="contained"
+							onPress={this.openKawalCovidTwitter}
+							color="#1DA1F2"
+							style={{ paddingVertical: 10 }}
+							labelStyle={{ color: 'white' }}
+						>
+							Twitter KawalCovid19
+						</Button>
+					</View>
 				</View>
 			</View>
 		);
